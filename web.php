1269a6fb24c4e9800ede3d42e78ce1f45a76b50b
coder_titan <64739704+Sahil0402@users.noreply.github.com>
2020-10-26 18:38:54 +0530

    Add files via upload
    Files uploaded

---
+++ web.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>
+    My links
+  </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="myapp.css">
+</head>
+
+<body class="bg-dark">
+  <div class="container">
+  <div class="jumbotron d-flex justify-content-center">
+        <svg width="368" height="94" viewBox="0 0 368 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0)">
+        <rect width="368" height="94" fill="#E14E4E"/>
+        <g filter="url(#filter0_d)">
+        <line y1="-13.5" x2="241.51" y2="-13.5" transform="matrix(0.793423 0.608671 -0.643692 0.765285 77.8478 -15)" stroke="black" stroke-width="27"/>
+        </g>
+        <path d="M98.5 69H64.9375V22.4688H76.9375V60H98.5V69ZM114.967 69H103.717V33.9062H114.967V69ZM115.279 28.9062H103.404V20.375H115.279V28.9062ZM159.434 69H148.121V51.5938C148.121 50.1771 148.049 48.7708 147.903 47.375C147.757 45.9583 147.507 44.9167 147.153 44.25C146.736 43.4792 146.121 42.9167 145.309 42.5625C144.517 42.2083 143.403 42.0312 141.965 42.0312C140.944 42.0312 139.903 42.1979 138.84 42.5312C137.799 42.8646 136.663 43.3958 135.434 44.125V69H124.184V33.9062H135.434V37.7812C137.434 36.2188 139.351 35.0208 141.184 34.1875C143.038 33.3542 145.09 32.9375 147.34 32.9375C151.132 32.9375 154.09 34.0417 156.215 36.25C158.361 38.4583 159.434 41.7604 159.434 46.1562V69ZM205.651 69H192.557L182.714 53.7188L179.651 57.4375V69H168.401V20.375H179.651V49.4062L191.776 33.9062H204.745L192.12 49.0312L205.651 69ZM240.43 57.875C240.43 61.5 238.805 64.4271 235.555 66.6562C232.305 68.8646 227.858 69.9688 222.212 69.9688C219.087 69.9688 216.253 69.6771 213.712 69.0938C211.17 68.5104 209.128 67.8646 207.587 67.1562V57.9062H208.618C209.18 58.3021 209.826 58.7396 210.555 59.2188C211.305 59.6771 212.358 60.1771 213.712 60.7188C214.878 61.1979 216.201 61.6146 217.68 61.9688C219.16 62.3021 220.743 62.4688 222.43 62.4688C224.639 62.4688 226.274 62.2292 227.337 61.75C228.399 61.25 228.93 60.5208 228.93 59.5625C228.93 58.7083 228.618 58.0938 227.993 57.7188C227.368 57.3229 226.18 56.9479 224.43 56.5938C223.597 56.4062 222.462 56.2083 221.024 56C219.608 55.7708 218.316 55.5 217.149 55.1875C213.962 54.3542 211.597 53.0625 210.055 51.3125C208.514 49.5417 207.743 47.3438 207.743 44.7188C207.743 41.3438 209.337 38.5417 212.524 36.3125C215.733 34.0625 220.108 32.9375 225.649 32.9375C228.274 32.9375 230.785 33.2083 233.18 33.75C235.597 34.2708 237.483 34.8333 238.837 35.4375V44.3125H237.868C236.201 43.1667 234.295 42.2396 232.149 41.5312C230.024 40.8021 227.868 40.4375 225.68 40.4375C223.868 40.4375 222.337 40.6875 221.087 41.1875C219.858 41.6875 219.243 42.3958 219.243 43.3125C219.243 44.1458 219.524 44.7812 220.087 45.2188C220.649 45.6562 221.972 46.0938 224.055 46.5312C225.201 46.7604 226.43 46.9896 227.743 47.2188C229.076 47.4271 230.41 47.6979 231.743 48.0312C234.701 48.8021 236.889 50.0208 238.305 51.6875C239.722 53.3333 240.43 55.3958 240.43 57.875ZM258.616 69H246.929V56.7188H258.616V69ZM279.49 69H268.24V33.9062H279.49V69ZM279.802 28.9062H267.927V20.375H279.802V28.9062ZM324.581 51.4688C324.581 57.2396 322.894 61.7917 319.519 65.125C316.165 68.4375 311.446 70.0938 305.363 70.0938C299.279 70.0938 294.55 68.4375 291.175 65.125C287.821 61.7917 286.144 57.2396 286.144 51.4688C286.144 45.6562 287.831 41.0938 291.206 37.7812C294.602 34.4688 299.321 32.8125 305.363 32.8125C311.488 32.8125 316.217 34.4792 319.55 37.8125C322.904 41.1458 324.581 45.6979 324.581 51.4688ZM310.738 60C311.467 59.1042 312.009 58.0312 312.363 56.7812C312.738 55.5104 312.925 53.7604 312.925 51.5312C312.925 49.4688 312.738 47.7396 312.363 46.3438C311.988 44.9479 311.467 43.8333 310.8 43C310.134 42.1458 309.331 41.5417 308.394 41.1875C307.456 40.8333 306.446 40.6562 305.363 40.6562C304.279 40.6562 303.311 40.8021 302.456 41.0938C301.623 41.3854 300.821 41.9688 300.05 42.8438C299.363 43.6562 298.811 44.7708 298.394 46.1875C297.998 47.6042 297.8 49.3854 297.8 51.5312C297.8 53.4479 297.977 55.1146 298.331 56.5312C298.686 57.9271 299.206 59.0521 299.894 59.9062C300.561 60.7188 301.352 61.3125 302.269 61.6875C303.206 62.0625 304.269 62.25 305.456 62.25C306.477 62.25 307.446 62.0833 308.363 61.75C309.3 61.3958 310.092 60.8125 310.738 60Z" fill="white"/>
+        </g>
+        <defs>
+        <filter id="filter0_d" x="73.8478" y="-35.6627" width="216.999" height="175.663" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+        </filter>
+        <clipPath id="clip0">
+        <rect width="368" height="94" fill="white"/>
+        </clipPath>
+        </defs>
+        </svg>
+    </div>
+      <div class="text-warning">Categories</div><br>
+
+      <div class="buttons">
+        <a href="index.html" targer="_parent" class="btn btn-info mx-2 my-1">Home</a>
+        <a href="web.php" class="btn btn-primary mx-2 my-1">Web development</a>
+        <a href="java.php" class="btn btn-outline-primary mx-2 my-1">Java</a>
+        <a href="Python.php" class="btn btn-outline-primary mx-2 my-1">Python</a>
+        <a href="Drive.php" class="btn btn-outline-primary mx-2 my-1">Drive links</a>
+      </div>
+
+      <div class="mod d-flex">
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-primary my-5 mx-2" data-toggle="modal" data-target="#exampleModal">
+                  Add
+                  </button>
+                  
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLabel">Add link</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <input type="text" name="title"  placeholder="Title" autocomplete="off" required>
+                          <input type="text" name="description" id="des" placeholder="Description" autocomplete="off" required>
+                          <input type="text" name="link_f" id="insert" placeholder="Paste your link" autocomplete="off" required><br>
+                          <input type="submit" name='btnd' class="btn btn-primary" value="Add Link">
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                      </div>
+                  </div>
+                </div>
+              </form>
+              <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                <button type="button" class="btn btn-danger my-5 mx-2" data-toggle="modal" data-target="#delModal">
+                  Delete
+                </button>
+
+                <div class="modal fade" id="delModal" tabindex="-1" role="dialog" aria-labelledby="delModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="delModalLabel">Delete link</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <input type="text" name="del_link_f" id="insert" placeholder="Paste your link" autocomplete="off" required><br>
+                          <input type="submit" class="btn btn-danger" name="btndel" value="Delete link">
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                      </div>
+                  </div>
+                </div>
+            </form>
+          </div>
+        <?php 
+                    function display()
+                    {
+                      global $con;
+                      $con = mysqli_connect("localhost","root","","own_app_by_me");
+                      if(!$con)
+                        echo mysqli_error($con);
+        
+                      $sql = "select * from web";
+                      $res = mysqli_query($GLOBALS['con'],$sql);
+                      
+                      echo '<div class="d-flex justify-content-around flex-wrap">';
+                      if(mysqli_num_rows($res) > 0)
+                      {
+                          while($rows = mysqli_fetch_assoc($res))
+                          {
+                     
+                            echo "<div class='card  my-5' style='width: 18rem;'>
+                            <div class='card-body'>
+                              <h5 class='card-title text-dark'>".$rows['title']."</h5>
+                              <p class='card-text text-dark'>".$rows['descr']."</p>
+                              <a href=".$rows['link']." class='card-link'>".$rows['link']."</a>
+                            </div>
+                          </div>";
+                         
+                          }
+                      }
+                      echo '</div>';   
+                    }
+          display(); 
+      ?>
+  
+</div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+
+</body>
+
+<?php
+  
+
+         
+          if(isset($_POST['btnd']) and $_POST['btnd'] == "Add Link")
+          {
+
+
+            $title = $_POST['title'];
+            $de = $_POST['description'];
+            $l = $_POST['link_f'];
+            $sql = "insert into web values ('$title','$de','$l');";
+            mysqli_query($con,$sql);
+          }    
+          
+          if(isset($_POST['btndel']) and $_POST['btndel'] == "Delete link")
+          {
+              $l = $_POST['del_link_f'];
+              $sql = "delete from web where link = '$l'";
+              mysqli_query($con,$sql);
+          }
+    ?>
+
+</html>
